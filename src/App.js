5483dbc0cb873ebec9e6963afbb5dbd1534b1259
shikhar1020 <shikhar.17.srivastava@st.niituniversity.in>
2020-10-21 20:08:25 +0530

    Posts with useState

--- src/App.js
+++ src/App.js
@@ -1,27 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-
 import Post from './post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: "shikharsangam",
+      caption: "Looove youu krishna",
+      imageurl: "https://i.pinimg.com/originals/b7/65/eb/b765eb49233959d3aaf790452986143d.jpg"
+    },
+    {
+      username:"sizi",
+      caption:"Aare krishna aap!",
+      imageurl:"https://media.tenor.com/images/6321fa6690d59b2f37c25ce0d271cb6c/tenor.gif"
+    },
+    {
+      username: "mystery_angel",
+      caption: "kaaaaaaaisse hooo!!",
+      imageurl: "https://i.pinimg.com/originals/b7/65/eb/b765eb49233959d3aaf790452986143d.jpg"
+    },
+    {
+      username: "shikharsangam_doobara",
+      caption: "mai toh masssst",
+      imageurl: "https://i.pinimg.com/originals/aa/cd/5b/aacd5b08af0c185a0aeae7211fe4bef0.jpg"
+    }
+  ]);
+    
   return (
-    <div className="app">        
-        
-        <div className="app_header">          
-          <img 
-            className="app_headerImage"
-            src="https://papajfunk.files.wordpress.com/2013/12/instagram-logo.png?w=640"
-            alt="Instagram"
-          />        
-        </div>
-        <div className="app_posts">  
-          <Post/>      
-          <Post/>   
-          <Post/>  
-          <Post/>  
-        </div>
+    <div className="app">            
+      <div className="app_header">          
+        <img 
+          className="app_headerImage"
+          src="https://papajfunk.files.wordpress.com/2013/12/instagram-logo.png?w=640"
+          alt="Instagram"
+        />        
+      </div>
+       
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageurl={post.imageurl}/>
+        ))
+      }
 
-    </div>
+    </div> 
   );
 }
 
--- src/post.js
+++ src/post.js
@@ -3,23 +3,24 @@ import './post.css';
 import Avatar from "@material-ui/core/Avatar";
 
 function post({username, caption, imageurl }) {
+    
     return (
         <div className="post">
             <div className="post_header">
                 <Avatar 
                     className="post_avatar"
                     alt="Krishna"
-                    src={imageurl}
+                    src="https://i.pinimg.com/originals/fc/ea/9f/fcea9f6961956cb431f752fef6ff2ed7.png"
                 />
                 <h4 className="post_headerUsername">{username}</h4>
             </div>
             <img 
                 className= "post_image" 
-                src="https://i.pinimg.com/564x/2b/4b/ea/2b4beaad4f09f39c48771d8363546079.jpg"
+                src={imageurl}
                 alt=""
             />
             <div className="post_footer">
-                <h4 className= "post_text"><strong>shikharsangam:</strong>{caption}</h4>
+                <h4 className= "post_text"><strong>shikharsangam:</strong> {caption} </h4>
             </div>
         </div>
 
