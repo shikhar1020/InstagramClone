44f1dd8ef26993cada800d27ef35b9cf44d1284e
shikhar1020 <shikhar.17.srivastava@st.niituniversity.in>
2020-10-21 22:43:12 +0530

    Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from "firebase";
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyD1q3sPb-5R3UP-_vDxyQ3wF0l_TIoHS8E",
+    authDomain: "instagram-clone-76c99.firebaseapp.com",
+    databaseURL: "https://instagram-clone-76c99.firebaseio.com",
+    projectId: "instagram-clone-76c99",
+    storageBucket: "instagram-clone-76c99.appspot.com",
+    messagingSenderId: "226767340752",
+    appId: "1:226767340752:web:2dde52ddec5eb0fef88ad5",
+    measurementId: "G-TNJBSKBK2Q"
+});
+
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+const storage = firebase.storage(); 
